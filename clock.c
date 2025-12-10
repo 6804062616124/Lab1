6b69608a65606f55ab17fdb45c88645e4020fb50
6804062616124 <s6804062616124@email.kmutnb.ac.th>
2025-12-10 13:46:57 +0700

    Add files via upload

---
+++ clock.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+	
+	int hour, min, sec;
+	
+	scanf("%d:%d:%d", &hour, &min, &sec);
+	
+	printf("hour:%d \nminute:%d \nsecond:%d", hour, min, sec);
+
+	return 0;
+}
